3576e420618218a2e3a9a84c61641b7f647abf25
Roham Koohestani <69460326+RebelOfDeath@users.noreply.github.com>
2021-05-24 17:36:51 +0200

    added section 6 files

---
+++ section6/section6/GlobalVars/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+//int age {18};            // global variables
+
+int main(){
+        
+//    int age {18};         //local variable ( these variables are looked for first by the compiler )
+    
+        return 0;
+}
---
+++ section6/section6/project1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+        cout << "Hello" ;
+        cout << " World" << endl;
+        
+        cout << "Hello world!" << endl;
+        cout << "Hello " << "world!" << endl;
+        cout << "Hello" << " world!\n";
+        cout << "Hello\nOut\nThere\n";
+        
+        return 0;
+        
+        }
