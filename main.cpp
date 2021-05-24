a312ecc7be18b1f24f4d4e196247ee82fc5998f8
Roham Koohestani <69460326+RebelOfDeath@users.noreply.github.com>
2021-05-24 18:20:37 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//please read the readme file because I have given all the information about this repository in there
+//hope you enjoy
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout << "Go check the readme file\n";
+  reutrn 0;
+}
